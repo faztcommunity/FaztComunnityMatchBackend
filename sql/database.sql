da002900e5a48c5ac2ddf5bc1a36f380b6da4896
Eslabi <deividelpaisa@hotmail.com>
2020-10-21 13:48:58 -0500

    configuration mysql added

---
+++ sql/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE fazt_match;
+
+CREATE TABLE register(
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    lastname VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(20) NOT NULL
+    created_at TIMESTAMP DEFAULT CURRENT_TIME
+);
+
