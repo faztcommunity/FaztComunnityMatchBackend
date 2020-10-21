9ce33b9cc61b05f9836818a82b805d82474a6b98
Eslabi <deividelpaisa@hotmail.com>
2020-10-21 17:25:02 -0500

    save to database correctly

--- src/controllers/register.controller.ts
+++ src/controllers/register.controller.ts
@@ -1,15 +1,17 @@
-import {Request, Response} from 'express'
+import {query, Request, Response} from 'express'
 import {connect} from '../database'
 import {User} from '../interface/IUser'
 
 export async function getUsers(req: Request, res: Response): Promise<Response>{
     const conn = await connect();
-    const register = await conn.query('SELECT * FROM register');
-    return res.json(register[0]);
+    const user = await conn.query('SELECT * FROM register');
+    return res.json(user[0]);
 };
 
 export async function createUser(req:Request, res: Response) {
     const newUser: User = req.body;
+    const conn = await connect();
+    await conn.query('INSERT INTO register SET ?', [newUser]);
     return res.json({
         message: 'Usuario Creado'
     });
--- src/interface/IUser.ts
+++ src/interface/IUser.ts
@@ -1,7 +1,7 @@
 export interface User {
     id?: string;
     name: string;
-    lastName: string;
+    last_name: string;
     email: string;
     password: string;
     created_at: Date;
